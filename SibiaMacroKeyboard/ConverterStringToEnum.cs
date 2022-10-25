7bade035ba5190d7b2a3a7193a7c66d7f89a5114
Pavel Kuzmin <psibia.p@gmail.com>
2022-10-25 23:57:20 +0700

    transferring the converter to the new class EnumConvert

---
+++ SibiaMacroKeyboard/ConverterStringToEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SibiaMacroKeyboard
+{
+    public static class ConverterStringToEnum
+    {
+        public static T ToEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+    }
+}
