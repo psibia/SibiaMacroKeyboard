04efb0761f6ac2e20f7e6978d82d97f716150b33
Pavel Kuzmin <psibia.p@gmail.com>
2022-10-26 01:47:20 +0700

    Add Unit Test

---
+++ SibiaMacroKeyboard.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SibiaMacroKeyboard.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SibiaMacroKeyboard.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ecb59af-489c-4c2f-962a-5cc412a8a0e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SibiaMacroKeyboard.Tests/SibiaMacroKeyboardTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SibiaMacroKeyboard.Tests
+{
+    [TestClass]
+    public class SibiaMacroKeyboardTests
+    {
+        [TestMethod]
+        public void ToEnum_SHIFT_return_16()
+        {
+            // arrange
+            string text = "SHIFT";
+            int expected = 16;
+
+
+            // action
+            int actual = Convert.ToInt32(ConverterStringToEnum.ToEnum<WindowsInput.Native.VirtualKeyCode>("SHIFT"));
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
